9d573cb3237554bbeb51d41e9b1c586ce4904913
willsen4 <willsenaa@hotamil.com>
2025-08-04 13:40:35 -0300

    update

--- SQL.sql
+++ SQL.sql
@@ -1,4 +1,6 @@
 CREATE DATABASE glpi_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER 'glpi_user'@'%' IDENTIFIED BY 'sua_senha_forte';
-GRANT ALL PRIVILEGES ON glpi_db.* TO 'glpi_user'@'%';
+CREATE USER 'glpi_user'@'%' IDENTIFIED BY 'sua_senha';
+CREATE USER 'glpi_web'@'%' IDENTIFIED BY 'sua_senha_forte';
+GRANT ALL PRIVILEGES ON glpi_db.* TO 'glpi_user'@'localhost'; -- Permissões para localhost
+GRANT ALL PRIVILEGES ON glpi_db.* TO 'glpi_web'@'%'; -- Permissões remotas
 FLUSH PRIVILEGES;
--- comandos.sh
+++ comandos.sh
@@ -16,7 +16,7 @@ docker exec -it mysql-app mysql -uroot -p
 
 SQL
 CREATE DATABASE glpi_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER 'glpi_user'@'%' IDENTIFIED BY 'Infra2025';
+CREATE USER 'glpi_user'@'%' IDENTIFIED BY 'sua_senha';
 GRANT ALL PRIVILEGES ON glpi_db.* TO 'glpi_user'@'%';
 FLUSH PRIVILEGES;
 
