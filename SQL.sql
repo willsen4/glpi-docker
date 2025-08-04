bc502db50b0f1ec749b374e18f00f508f1e932a4
William de Oliveira Sena <willsenaa@hotmail.com>
2025-08-04 14:08:10 -0300

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -1,6 +1,4 @@
 CREATE DATABASE glpi_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER 'glpi_user'@'localhost' IDENTIFIED BY 'sua_senha';
-CREATE USER 'glpi_web'@'%' IDENTIFIED BY 'sua_senha_forte';
-GRANT ALL PRIVILEGES ON glpi_db.* TO 'glpi_user'@'localhost'; -- Permissões para localhost
-GRANT ALL PRIVILEGES ON glpi_db.* TO 'glpi_web'@'%'; -- Permissões remotas
+CREATE USER 'glpi_user'@'%' IDENTIFIED BY 'sua_senha_forte';
+GRANT ALL PRIVILEGES ON glpi_db.* TO 'glpi_user'@'%';
 FLUSH PRIVILEGES;
